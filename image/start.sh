23e5231e9699ee1bb15860b319a108c150aa3b18
Matthew Landauer <mlandauer@gmail.com>
2019-08-06 15:39:51 +1000

    Generalise to other languages

--- image/start.sh
+++ image/start.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-# Just for the time being assume we're running a ruby scraper
-# TODO: Check what scraper language we're running and kick off in the appropriate way
-bundle exec ruby -r/usr/local/lib/prerun.rb scraper.rb
+if [ -f "scraper.rb" ]; then
+  bundle exec ruby -r/usr/local/lib/prerun.rb scraper.rb
+elif [ -f "scraper.php" ]; then
+  php -d include_path=.:/app/vendor/openaustralia/scraperwiki scraper.php
+elif [ -f "scraper.py" ]; then
+  # -u turns off buffering for stdout and stderr
+  python: "python -u scraper.py",
+elif [ -f "scraper.pl" ]; then
+  perl -Mlib=/app/local/lib/perl5 scraper.pl
+elif [ -f "scraper.js" ]; then
+  node --expose-gc scraper.js
+else
+  # TODO: Make a better error message
+  echo "Can'f find scraper to run"
+  exit 1
+fi
