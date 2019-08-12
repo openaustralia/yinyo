ff0c8428bc5131da12d3dd83ae15758b36e137ba
Matthew Landauer <mlandauer@gmail.com>
2019-08-12 10:42:14 +1000

    Now access kubernetes api from inside the cluster

--- server/main.go
+++ server/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -11,29 +13,12 @@ import (
 
 // The body of the request should contain the tarred & gzipped code
 // to be run
-// func run(w http.ResponseWriter, r *http.Request) {
-// 	// scraperID := mux.Vars(r)["id"]
-// 	// outputFilename := r.Header.Get("Clay-Scraper-Output")
-//
-// 	// For testing purposes let's just list the names of all the pods
-// 	// currently running
-// 	config, err := rest.InClusterConfig()
-// 	if err != nil {
-// 		panic(err.Error())
-// 	}
-// 	clientset, err := kubernetes.NewForConfig(config)
-// 	if err != nil {
-// 		panic(err.Error())
-// 	}
-// 	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
-// 	if err != nil {
-// 		panic(err.Error())
-// 	}
-// 	fmt.Fprintf(w, "There are %d pods in the cluster\n", len(pods.Items))
-// 	// fmt.Fprintf(w, "scraperID: %s, outputFilename: %s", scraperID, outputFilename)
-// }
+func run(w http.ResponseWriter, r *http.Request) {
+	// scraperID := mux.Vars(r)["id"]
+	// outputFilename := r.Header.Get("Clay-Scraper-Output")
 
-func main() {
+	// For testing purposes let's just list the names of all the pods
+	// currently running
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(err.Error())
@@ -42,16 +27,16 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	for {
-		pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-		time.Sleep(10 * time.Second)
+	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
 	}
+	fmt.Fprintf(w, "There are %d pods in the cluster\n", len(pods.Items))
+	// fmt.Fprintf(w, "scraperID: %s, outputFilename: %s", scraperID, outputFilename)
+}
 
-	// router := mux.NewRouter().StrictSlash(true)
-	// router.HandleFunc("/scrapers/{id}/run", run).Methods("PUT")
-	// log.Fatal(http.ListenAndServe(":8080", router))
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/scrapers/{id}/run", run).Methods("PUT")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
