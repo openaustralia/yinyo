3535264a6b15781fa2b6a2e4c9526005f6917f71
Matthew Landauer <mlandauer@gmail.com>
2019-08-12 10:05:35 +1000

    Little test of accessing kubernetes api inside pod

--- server/go.mod
+++ server/go.mod
@@ -4,5 +4,6 @@ go 1.12
 
 require (
 	github.com/gorilla/mux v1.7.3
-	k8s.io/client-go v0.0.0-20190620085101-78d2af792bab // indirect
+	k8s.io/apimachinery v0.0.0-20190612205821-1799e75a0719
+	k8s.io/client-go v0.0.0-20190620085101-78d2af792bab
 )
--- server/main.go
+++ server/main.go
@@ -2,23 +2,56 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
+	"time"
 
-	"github.com/gorilla/mux"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 // The body of the request should contain the tarred & gzipped code
 // to be run
-func run(w http.ResponseWriter, r *http.Request) {
-	scraperID := mux.Vars(r)["id"]
-	outputFilename := r.Header.Get("Clay-Scraper-Output")
-
-	fmt.Fprintf(w, "scraperID: %s, outputFilename: %s", scraperID, outputFilename)
-}
+// func run(w http.ResponseWriter, r *http.Request) {
+// 	// scraperID := mux.Vars(r)["id"]
+// 	// outputFilename := r.Header.Get("Clay-Scraper-Output")
+//
+// 	// For testing purposes let's just list the names of all the pods
+// 	// currently running
+// 	config, err := rest.InClusterConfig()
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+// 	clientset, err := kubernetes.NewForConfig(config)
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+// 	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+// 	fmt.Fprintf(w, "There are %d pods in the cluster\n", len(pods.Items))
+// 	// fmt.Fprintf(w, "scraperID: %s, outputFilename: %s", scraperID, outputFilename)
+// }
 
 func main() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/scrapers/{id}/run", run).Methods("PUT")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	for {
+		pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+		time.Sleep(10 * time.Second)
+	}
+
+	// router := mux.NewRouter().StrictSlash(true)
+	// router.HandleFunc("/scrapers/{id}/run", run).Methods("PUT")
+	// log.Fatal(http.ListenAndServe(":8080", router))
 }
