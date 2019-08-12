b5bdc7bdfbedeed9a6a6c9a33b42b73d785a558f
Matthew Landauer <mlandauer@gmail.com>
2019-08-12 13:14:13 +1000

    Creates a job in response to a web request

--- server/go.mod
+++ server/go.mod
@@ -4,6 +4,9 @@ go 1.12
 
 require (
 	github.com/gorilla/mux v1.7.3
+	github.com/keegancsmith/rpc v1.1.0 // indirect
+	github.com/stamblerre/gocode v0.0.0-20190327203809-810592086997 // indirect
+	k8s.io/api v0.0.0-20190620084959-7cf5895f2711
 	k8s.io/apimachinery v0.0.0-20190612205821-1799e75a0719
 	k8s.io/client-go v0.0.0-20190620085101-78d2af792bab
 )
--- server/main.go
+++ server/main.go
@@ -6,19 +6,22 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	batchv1 "k8s.io/api/batch/v1"
+	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
 
+func int32Ptr(i int32) *int32 { return &i }
+func int64Ptr(i int64) *int64 { return &i }
+
 // The body of the request should contain the tarred & gzipped code
 // to be run
 func run(w http.ResponseWriter, r *http.Request) {
-	// scraperID := mux.Vars(r)["id"]
-	// outputFilename := r.Header.Get("Clay-Scraper-Output")
+	scraperName := mux.Vars(r)["id"]
+	scraperOutput := r.Header.Get("Clay-Scraper-Output")
 
-	// For testing purposes let's just list the names of all the pods
-	// currently running
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(err.Error())
@@ -27,11 +30,50 @@ func run(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err.Error())
 	}
-	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
-	if err != nil {
-		panic(err.Error())
+	jobsClient := clientset.BatchV1().Jobs("default")
+	job := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: scraperName,
+		},
+		Spec: batchv1.JobSpec{
+			BackoffLimit: int32Ptr(0),
+			// Let this run for a maximum of 24 hours
+			ActiveDeadlineSeconds: int64Ptr(86400),
+			Template: apiv1.PodTemplateSpec{
+				Spec: apiv1.PodSpec{
+					RestartPolicy: "Never",
+					Containers: []apiv1.Container{
+						{
+							Name:    scraperName,
+							Image:   "clay",
+							Command: []string{"/bin/run.sh", scraperName, scraperOutput},
+							// Doing this so that we use the local image while we're developing
+							ImagePullPolicy: "Never",
+							Env: []apiv1.EnvVar{
+								{
+									Name: "CLAY_RUN_TOKEN",
+									ValueFrom: &apiv1.EnvVarSource{
+										SecretKeyRef: &apiv1.SecretKeySelector{
+											LocalObjectReference: apiv1.LocalObjectReference{
+												Name: scraperName,
+											},
+											Key: "run_token",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
 	}
-	fmt.Fprintf(w, "There are %d pods in the cluster\n", len(pods.Items))
+	result1, err1 := jobsClient.Create(job)
+	if err != nil {
+		fmt.Println(err1)
+		panic(err1)
+	}
+	fmt.Fprintf(w, "Created job %q.\n", result1)
 	// fmt.Fprintf(w, "scraperID: %s, outputFilename: %s", scraperID, outputFilename)
 }
 
