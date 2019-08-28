16f028fe2a803d0659da937f8ca39eab232eaa28
Matthew Landauer <mlandauer@gmail.com>
2019-08-28 15:23:41 +1000

    Extract usage measurement into separate command

--- images/clay-scraper/run.sh
+++ images/clay-scraper/run.sh
@@ -22,48 +22,6 @@ export CLAY_SERVER_URL=clay-server.clay-system:8080
 # Turns on debugging output in herokuish
 # export TRACE=true
 
-extract_value() {
-  local filename="$1"
-  local label="$2"
-  local line=$(grep "$label" "$filename")
-  echo "${line#*$label: }"
-}
-
-stats() {
-  local filename="$1"
-  local rx_bytes="$2"
-  local tx_bytes="$3"
-
-  local wall_time_formatted=$(extract_value "$filename" "Elapsed (wall clock) time (h:mm:ss or m:ss)")
-  local user_time=$(extract_value "$filename" "User time (seconds)")
-  local system_time=$(extract_value "$filename" "System time (seconds)")
-  local max_rss=$(extract_value "$filename" "Maximum resident set size (kbytes)")
-  local page_size=$(extract_value "$filename" "Page size (bytes)")
-
-  local part1=$(echo $wall_time_formatted | cut -d ':' -f 1)
-  local part2=$(echo $wall_time_formatted | cut -d ':' -f 2)
-  local part3=$(echo $wall_time_formatted | cut -d ':' -f 3)
-
-  # If part3 is empty (time is in m:ss)
-  if [ -z "$part3" ]; then
-    local wall_time=$(echo "$part1 * 60.0 + $part2" | bc)
-  # Else time is in h:mm:ss
-  else
-    local wall_time=$(echo "($part1 * 60.0 + $part2) * 60.0 + $part3" | bc)
-  fi
-
-  local cpu_time=$(echo "$user_time + $system_time" | bc)
-
-  # There's a bug in GNU time 1.7 which wrongly reports the maximum resident
-  # set size on the version of Ubuntu that we're using.
-  # See https://groups.google.com/forum/#!topic/gnu.utils.help/u1MOsHL4bhg
-  # Let's fix it up
-  local max_rss=$(echo "$max_rss * 1024 / $page_size" | bc)
-
-  # Returns result as JSON
-  echo "{\"wall_time\": $wall_time, \"cpu_time\": $cpu_time, \"max_rss\": $max_rss, \"network_in\": $rx_bytes, \"network_out\": $tx_bytes}"
-}
-
 # TODO: Probably don't want to do this as root
 
 cd /tmp || exit
@@ -74,39 +32,19 @@ cp /usr/local/lib/Procfile /tmp/app/Procfile
 
 (/bin/clay.sh get "$RUN_NAME" "$CLAY_RUN_TOKEN" cache | tar xzf - 2> /dev/null) || true
 
-snapshot=$(ip -s -j link show eth0)
-rx_bytes_before=$(echo "$snapshot" | jq ".[0].stats64.rx.bytes")
-tx_bytes_before=$(echo "$snapshot" | jq ".[0].stats64.tx.bytes")
-
 # This fairly hideous construction pipes stdout and stderr to seperate commands
-{ /usr/bin/time -v -o /tmp/time_output_build.txt /bin/herokuish buildpack build 2>&3 | /bin/clay.sh send-logs "$RUN_NAME" "$CLAY_RUN_TOKEN" stdout; } 3>&1 1>&2 | /bin/clay.sh send-logs "$RUN_NAME" "$CLAY_RUN_TOKEN" stderr
-
-snapshot=$(ip -s -j link show eth0)
-rx_bytes_after=$(echo "$snapshot" | jq ".[0].stats64.rx.bytes")
-tx_bytes_after=$(echo "$snapshot" | jq ".[0].stats64.tx.bytes")
-rx_bytes_build=$(echo "$rx_bytes_after - $rx_bytes_before" | bc)
-tx_bytes_build=$(echo "$tx_bytes_after - $tx_bytes_before" | bc)
+{ /bin/usage.sh /tmp/usage_build.json /bin/herokuish buildpack build 2>&3 | /bin/clay.sh send-logs "$RUN_NAME" "$CLAY_RUN_TOKEN" stdout; } 3>&1 1>&2 | /bin/clay.sh send-logs "$RUN_NAME" "$CLAY_RUN_TOKEN" stderr
 
 # TODO: If the build fails then it shouldn't try to run the scraper but it should record stats
 
 tar -zcf - cache | /bin/clay.sh put "$RUN_NAME" "$CLAY_RUN_TOKEN" cache
 
-snapshot=$(ip -s -j link show eth0)
-rx_bytes_before=$(echo "$snapshot" | jq ".[0].stats64.rx.bytes")
-tx_bytes_before=$(echo "$snapshot" | jq ".[0].stats64.tx.bytes")
-
-{ /usr/bin/time -v -o /tmp/time_output_run.txt /bin/herokuish procfile start scraper 2>&3 | /bin/clay.sh send-logs "$RUN_NAME" "$CLAY_RUN_TOKEN" stdout; } 3>&1 1>&2 | /bin/clay.sh send-logs "$RUN_NAME" "$CLAY_RUN_TOKEN" stderr
-
-snapshot=$(ip -s -j link show eth0)
-rx_bytes_after=$(echo "$snapshot" | jq ".[0].stats64.rx.bytes")
-tx_bytes_after=$(echo "$snapshot" | jq ".[0].stats64.tx.bytes")
-rx_bytes_run=$(echo "$rx_bytes_after - $rx_bytes_before" | bc)
-tx_bytes_run=$(echo "$tx_bytes_after - $tx_bytes_before" | bc)
+{ /bin/usage.sh /tmp/usage_run.json /bin/herokuish procfile start scraper 2>&3 | /bin/clay.sh send-logs "$RUN_NAME" "$CLAY_RUN_TOKEN" stdout; } 3>&1 1>&2 | /bin/clay.sh send-logs "$RUN_NAME" "$CLAY_RUN_TOKEN" stderr
 
 exit_code=${PIPESTATUS[0]}
 
-build_statistics=$(stats /tmp/time_output_build.txt $rx_bytes_build $tx_bytes_build)
-run_statistics=$(stats /tmp/time_output_run.txt $rx_bytes_run $tx_bytes_run)
+build_statistics=$(cat /tmp/usage_build.json)
+run_statistics=$(cat /tmp/usage_run.json)
 overall_stats="{\"exit_code\": $exit_code, \"usage\": {\"build\": $build_statistics, \"run\": $run_statistics}}"
 echo $overall_stats | /bin/clay.sh put "$RUN_NAME" "$CLAY_RUN_TOKEN" exit-data
 
---
+++ images/clay-scraper/usage.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+set -o pipefail
+
+if [ $# == 0 ]; then
+  echo "Run an external command and measure its resource usage"
+  echo "Usage: $0 output_file command"
+  echo "e.g. $0 usage.json sleep 2"
+  exit 1
+fi
+
+extract_value() {
+  local filename="$1"
+  local label="$2"
+  local line=$(grep "$label" "$filename")
+  echo "${line#*$label: }"
+}
+
+stats() {
+  local filename="$1"
+  local rx_bytes="$2"
+  local tx_bytes="$3"
+
+  local wall_time_formatted=$(extract_value "$filename" "Elapsed (wall clock) time (h:mm:ss or m:ss)")
+  local user_time=$(extract_value "$filename" "User time (seconds)")
+  local system_time=$(extract_value "$filename" "System time (seconds)")
+  local max_rss=$(extract_value "$filename" "Maximum resident set size (kbytes)")
+  local page_size=$(extract_value "$filename" "Page size (bytes)")
+
+  local part1=$(echo $wall_time_formatted | cut -d ':' -f 1)
+  local part2=$(echo $wall_time_formatted | cut -d ':' -f 2)
+  local part3=$(echo $wall_time_formatted | cut -d ':' -f 3)
+
+  # If part3 is empty (time is in m:ss)
+  if [ -z "$part3" ]; then
+    local wall_time=$(echo "$part1 * 60.0 + $part2" | bc)
+  # Else time is in h:mm:ss
+  else
+    local wall_time=$(echo "($part1 * 60.0 + $part2) * 60.0 + $part3" | bc)
+  fi
+
+  local cpu_time=$(echo "$user_time + $system_time" | bc)
+
+  # There's a bug in GNU time 1.7 which wrongly reports the maximum resident
+  # set size on the version of Ubuntu that we're using.
+  # See https://groups.google.com/forum/#!topic/gnu.utils.help/u1MOsHL4bhg
+  # Let's fix it up
+  local max_rss=$(echo "$max_rss * 1024 / $page_size" | bc)
+
+  # Returns result as JSON
+  echo "{\"wall_time\": $wall_time, \"cpu_time\": $cpu_time, \"max_rss\": $max_rss, \"network_in\": $rx_bytes, \"network_out\": $tx_bytes}"
+}
+
+filename=$1
+shift
+
+snapshot=$(ip -s -j link show eth0)
+rx_bytes_before=$(echo "$snapshot" | jq ".[0].stats64.rx.bytes")
+tx_bytes_before=$(echo "$snapshot" | jq ".[0].stats64.tx.bytes")
+
+/usr/bin/time -v -o /tmp/time_output.txt $@
+
+snapshot=$(ip -s -j link show eth0)
+rx_bytes_after=$(echo "$snapshot" | jq ".[0].stats64.rx.bytes")
+tx_bytes_after=$(echo "$snapshot" | jq ".[0].stats64.tx.bytes")
+rx_bytes=$(echo "$rx_bytes_after - $rx_bytes_before" | bc)
+tx_bytes=$(echo "$tx_bytes_after - $tx_bytes_before" | bc)
+
+stats /tmp/time_output.txt $rx_bytes $tx_bytes > $filename
+rm /tmp/time_output.txt
