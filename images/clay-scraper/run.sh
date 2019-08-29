a374ecc0d4f52a04a7ec032e2a87b433791df758
Matthew Landauer <mlandauer@gmail.com>
2019-08-30 09:24:01 +1000

    Store cache without the top level directory (similar to app)

--- images/clay-scraper/run.sh
+++ images/clay-scraper/run.sh
@@ -3,6 +3,8 @@
 # exit when any command fails
 set -e
 set -o pipefail
+# Enable globbing for hidden files
+shopt -s dotglob
 
 if [ $# == 0 ]; then
   echo "Compiles and runs a scraper"
@@ -26,19 +28,22 @@ export CLAY_SERVER_URL=clay-server.clay-system:8080
 
 cd /tmp || exit
 
-mkdir app
+mkdir -p app cache
+
 /bin/clay.sh get "$RUN_NAME" "$CLAY_RUN_TOKEN" app | tar xzf - -C app
 
 cp /usr/local/lib/Procfile /tmp/app/Procfile
 
-(/bin/clay.sh get "$RUN_NAME" "$CLAY_RUN_TOKEN" cache | tar xzf - 2> /dev/null) || true
+(/bin/clay.sh get "$RUN_NAME" "$CLAY_RUN_TOKEN" cache | tar xzf - -C cache 2> /dev/null) || true
 
 # This fairly hideous construction pipes stdout and stderr to seperate commands
 { /bin/usage.sh /tmp/usage_build.json /bin/herokuish buildpack build 2>&3 | /bin/clay.sh send-logs "$RUN_NAME" "$CLAY_RUN_TOKEN" stdout; } 3>&1 1>&2 | /bin/clay.sh send-logs "$RUN_NAME" "$CLAY_RUN_TOKEN" stderr
 
 # TODO: If the build fails then it shouldn't try to run the scraper but it should record stats
 
-tar -zcf - cache | /bin/clay.sh put "$RUN_NAME" "$CLAY_RUN_TOKEN" cache
+cd cache
+tar -zcf - * | /bin/clay.sh put "$RUN_NAME" "$CLAY_RUN_TOKEN" cache
+cd ..
 
 { /bin/usage.sh /tmp/usage_run.json /bin/herokuish procfile start scraper 2>&3 | /bin/clay.sh send-logs "$RUN_NAME" "$CLAY_RUN_TOKEN" stdout; } 3>&1 1>&2 | /bin/clay.sh send-logs "$RUN_NAME" "$CLAY_RUN_TOKEN" stderr
 
