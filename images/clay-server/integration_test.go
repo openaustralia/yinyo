e01a7bea37b6ae013752d60fff448cad92f17f29
Matthew Landauer <mlandauer@gmail.com>
2019-09-02 15:19:03 +1000

    Add simple integration test for hello response from server

---
+++ images/clay-server/integration_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// First go at doing end-to-end testing of the system
+func TestServerHello(t *testing.T) {
+	// This assumes the API is accessible at http://localhost:8080
+	resp, err := http.Get("http://localhost:8080")
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(resp.Header)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Equal(t, []string{"text/plain; charset=utf-8"}, resp.Header["Content-Type"])
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, "Hello from Clay!\n", string(b))
+}
