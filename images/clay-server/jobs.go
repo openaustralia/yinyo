b0c1f4dceae231997d112ffe98bd17c0205851c5
Matthew Landauer <mlandauer@gmail.com>
2019-10-03 10:23:37 +1000

    By ignoring trying to delete a job that doesn't exist we make the cleanup work better in the tests

--- images/clay-server/jobs.go
+++ images/clay-server/jobs.go
@@ -3,6 +3,7 @@ package main
 import (
 	batchv1 "k8s.io/api/batch/v1"
 	apiv1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
@@ -83,5 +84,12 @@ func deleteJob(clientset *kubernetes.Clientset, runName string) error {
 	err := jobsClient.Delete(runName, &metav1.DeleteOptions{
 		PropagationPolicy: &deletePolicy,
 	})
-	return err
+	if err != nil {
+		// Don't error if it's just that the job doesn't exist
+		if err.(*apierrors.StatusError).ErrStatus.Reason == metav1.StatusReasonNotFound {
+			return nil
+		}
+		return err
+	}
+	return nil
 }
