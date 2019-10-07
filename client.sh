2552effa57491f5deba117ea2395703388bad8e3
Matthew Landauer <mlandauer@gmail.com>
2019-10-07 11:27:26 +1100

    Write the output to the same directory as the local scraper. Fixes #32

--- client.sh
+++ client.sh
@@ -13,6 +13,7 @@ usage() {
   echo "Usage: $0 scraper_directory"
   echo ""
   echo "e.g. $0 test/scrapers/test-python"
+  echo "The output is written to the same local directory at the end"
   exit 1
 }
 
@@ -29,9 +30,6 @@ scraper_name=$1
 CLAY_SERVER_URL=http://localhost:8080
 export CLAY_SERVER_URL
 
-rm -rf app
-cp -R "$scraper_name" app
-
 create_result=$(./build/package/clay-scraper/clay.sh create "$scraper_name")
 run_name=$(echo "$create_result" | jq -r ".run_name")
 run_token=$(echo "$create_result" | jq -r ".run_token")
@@ -39,10 +37,9 @@ run_token=$(echo "$create_result" | jq -r ".run_token")
 # We want the tar to have the scraper files at its root
 # Note that this doesn't include hidden files currently. Do we want this?
 dir=$(pwd)
-cd app
+cd "$scraper_name"
 tar -zcf - * | "$dir/build/package/clay-scraper/clay.sh" put "$run_name" "$run_token" app
 cd "$dir"
-rm -rf app
 
 (cat "assets/client-storage/cache/$scraper_name.tgz" 2> /dev/null | ./build/package/clay-scraper/clay.sh put "$run_name" "$run_token" cache) || true
 ./build/package/clay-scraper/clay.sh start "$run_name" "$run_token" data.sqlite SCRAPER_NAME "$scraper_name"
@@ -54,8 +51,7 @@ fi
 
 ./build/package/clay-scraper/clay.sh events "$run_name" "$run_token" | jq -r 'select(has("log")) | .log'
 # Get the sqlite database from clay and save it away
-mkdir -p $(dirname "assets/client-storage/db/$scraper_name")
-./build/package/clay-scraper/clay.sh get "$run_name" "$run_token" output > "assets/client-storage/db/$scraper_name.sqlite"
+./build/package/clay-scraper/clay.sh get "$run_name" "$run_token" output > "$scraper_name/data.sqlite"
 mkdir -p $(dirname "assets/client-storage/cache/$scraper_name")
 ./build/package/clay-scraper/clay.sh get "$run_name" "$run_token" cache > "assets/client-storage/cache/$scraper_name.tgz"
 echo "exit data returned by clay:"
