0a7b197a32b35dea651dfe5ef135946e5c67a41d
Matthew Landauer <mlandauer@gmail.com>
2019-10-07 11:28:48 +1100

    Rename variable

--- client.sh
+++ client.sh
@@ -24,25 +24,25 @@ fi
 # Get rid of the parsed options so far
 shift $((OPTIND-1))
 
-scraper_name=$1
+scraper_directory=$1
 
 # This environment variable is used by clay.sh
 CLAY_SERVER_URL=http://localhost:8080
 export CLAY_SERVER_URL
 
-create_result=$(./build/package/clay-scraper/clay.sh create "$scraper_name")
+create_result=$(./build/package/clay-scraper/clay.sh create "$scraper_directory")
 run_name=$(echo "$create_result" | jq -r ".run_name")
 run_token=$(echo "$create_result" | jq -r ".run_token")
 
 # We want the tar to have the scraper files at its root
 # Note that this doesn't include hidden files currently. Do we want this?
 dir=$(pwd)
-cd "$scraper_name"
+cd "$scraper_directory"
 tar -zcf - * | "$dir/build/package/clay-scraper/clay.sh" put "$run_name" "$run_token" app
 cd "$dir"
 
-(cat "assets/client-storage/cache/$scraper_name.tgz" 2> /dev/null | ./build/package/clay-scraper/clay.sh put "$run_name" "$run_token" cache) || true
-./build/package/clay-scraper/clay.sh start "$run_name" "$run_token" data.sqlite SCRAPER_NAME "$scraper_name"
+(cat "assets/client-storage/cache/$scraper_directory.tgz" 2> /dev/null | ./build/package/clay-scraper/clay.sh put "$run_name" "$run_token" cache) || true
+./build/package/clay-scraper/clay.sh start "$run_name" "$run_token" data.sqlite SCRAPER_NAME "$scraper_directory"
 
 if [ "$run_token" = "" ]; then
   echo "There was an error starting the scraper"
@@ -51,9 +51,9 @@ fi
 
 ./build/package/clay-scraper/clay.sh events "$run_name" "$run_token" | jq -r 'select(has("log")) | .log'
 # Get the sqlite database from clay and save it away
-./build/package/clay-scraper/clay.sh get "$run_name" "$run_token" output > "$scraper_name/data.sqlite"
-mkdir -p $(dirname "assets/client-storage/cache/$scraper_name")
-./build/package/clay-scraper/clay.sh get "$run_name" "$run_token" cache > "assets/client-storage/cache/$scraper_name.tgz"
+./build/package/clay-scraper/clay.sh get "$run_name" "$run_token" output > "$scraper_directory/data.sqlite"
+mkdir -p $(dirname "assets/client-storage/cache/$scraper_directory")
+./build/package/clay-scraper/clay.sh get "$run_name" "$run_token" cache > "assets/client-storage/cache/$scraper_directory.tgz"
 echo "exit data returned by clay:"
 ./build/package/clay-scraper/clay.sh get "$run_name" "$run_token" exit-data |  jq .
 ./build/package/clay-scraper/clay.sh delete "$run_name" "$run_token"
