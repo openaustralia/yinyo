404d37a0e88640b6e960f99a82ad3a2c824ca2c0
Matthew Landauer <mlandauer@gmail.com>
2019-10-21 13:07:29 +1100

    Small refactor

--- build/package/clay-scraper/run.sh
+++ build/package/clay-scraper/run.sh
@@ -24,21 +24,21 @@ CLAY_SERVER_URL=clay-server.clay-system:8080
 # TODO: Probably don't want to do this as root
 
 started() {
-  data=$(jq -c -n --arg log "$line" --arg stage "$3" '{stage: $stage, type: "started"}')
-  curl -s -X POST -H "Authorization: Bearer $2" -H "Content-Type: application/json" "$CLAY_SERVER_URL/runs/$1/events" -d "$data"
+  data=$(jq -c -n --arg stage "$1" '{stage: $stage, type: "started"}')
+  curl -s -X POST -H "Authorization: Bearer $CLAY_INTERNAL_RUN_TOKEN" -H "Content-Type: application/json" "$CLAY_SERVER_URL/runs/$RUN_NAME/events" -d "$data"
 }
 
 finished() {
-  data=$(jq -c -n --arg log "$line" --arg stage "$3" '{stage: $stage, type: "finished"}')
-  curl -s -X POST -H "Authorization: Bearer $2" -H "Content-Type: application/json" "$CLAY_SERVER_URL/runs/$1/events" -d "$data"
+  data=$(jq -c -n --arg stage "$1" '{stage: $stage, type: "finished"}')
+  curl -s -X POST -H "Authorization: Bearer $CLAY_INTERNAL_RUN_TOKEN" -H "Content-Type: application/json" "$CLAY_SERVER_URL/runs/$RUN_NAME/events" -d "$data"
 }
 
 get() {
-  curl -s -H "Authorization: Bearer $2" "$CLAY_SERVER_URL/runs/$1/$3"
+  curl -s -H "Authorization: Bearer $CLAY_INTERNAL_RUN_TOKEN" "$CLAY_SERVER_URL/runs/$RUN_NAME/$1"
 }
 
 put() {
-  curl -s -X PUT -H "Authorization: Bearer $2" --data-binary @- --no-buffer "$CLAY_SERVER_URL/runs/$1/$3"
+  curl -s -X PUT -H "Authorization: Bearer $CLAY_INTERNAL_RUN_TOKEN" --data-binary @- --no-buffer "$CLAY_SERVER_URL/runs/$RUN_NAME/$1"
 }
 
 send-logs() {
@@ -46,50 +46,50 @@ send-logs() {
   while IFS= read -r text ;
   do
     # Send as json
-    data=$(jq -c -n --arg text "$text" --arg stage "$3" --arg stream "$4" '{stage: $stage, type: "log", stream: $stream, text: $text}')
-    curl -s -X POST -H "Authorization: Bearer $2" -H "Content-Type: application/json" "$CLAY_SERVER_URL/runs/$1/events" -d "$data"
+    data=$(jq -c -n --arg text "$text" --arg stage "$1" --arg stream "$2" '{stage: $stage, type: "log", stream: $stream, text: $text}')
+    curl -s -X POST -H "Authorization: Bearer $CLAY_INTERNAL_RUN_TOKEN" -H "Content-Type: application/json" "$CLAY_SERVER_URL/runs/$RUN_NAME/events" -d "$data"
   done
 }
 
 send-event() {
-  curl -s -X POST -H "Authorization: Bearer $2" -H "Content-Type: application/json" "$CLAY_SERVER_URL/runs/$1/events" -d "$3"
+  curl -s -X POST -H "Authorization: Bearer $CLAY_INTERNAL_RUN_TOKEN" -H "Content-Type: application/json" "$CLAY_SERVER_URL/runs/$RUN_NAME/events" -d "$1"
 }
 
-started "$RUN_NAME" "$CLAY_INTERNAL_RUN_TOKEN" build
+started build
 
 cd /tmp || exit
 
 mkdir -p app cache
 
-get "$RUN_NAME" "$CLAY_INTERNAL_RUN_TOKEN" app | tar xzf - -C app
+get app | tar xzf - -C app
 
 cp /usr/local/lib/Procfile /tmp/app/Procfile
 
-(get "$RUN_NAME" "$CLAY_INTERNAL_RUN_TOKEN" cache | tar xzf - -C cache 2> /dev/null) || true
+(get cache | tar xzf - -C cache 2> /dev/null) || true
 
 # This fairly hideous construction pipes stdout and stderr to seperate commands
-{ /bin/usage.sh /tmp/usage_build.json /bin/herokuish buildpack build 2>&3 | send-logs "$RUN_NAME" "$CLAY_INTERNAL_RUN_TOKEN" build stdout; } 3>&1 1>&2 | send-logs "$RUN_NAME" "$CLAY_INTERNAL_RUN_TOKEN" build stderr
+{ /bin/usage.sh /tmp/usage_build.json /bin/herokuish buildpack build 2>&3 | send-logs build stdout; } 3>&1 1>&2 | send-logs build stderr
 
 cd cache
-tar -zcf - * | put "$RUN_NAME" "$CLAY_INTERNAL_RUN_TOKEN" cache
+tar -zcf - * | put cache
 cd ..
 
-finished "$RUN_NAME" "$CLAY_INTERNAL_RUN_TOKEN" build
+finished build
 
 # TODO: Factor out common code from the build and run
-started "$RUN_NAME" "$CLAY_INTERNAL_RUN_TOKEN" run
-{ /bin/usage.sh /tmp/usage_run.json /bin/herokuish procfile start scraper 2>&3 | send-logs "$RUN_NAME" "$CLAY_INTERNAL_RUN_TOKEN" run stdout; } 3>&1 1>&2 | send-logs "$RUN_NAME" "$CLAY_INTERNAL_RUN_TOKEN" run stderr
+started run
+{ /bin/usage.sh /tmp/usage_run.json /bin/herokuish procfile start scraper 2>&3 | send-logs run stdout; } 3>&1 1>&2 | send-logs run stderr
 
 exit_code=${PIPESTATUS[0]}
 
 build_statistics=$(cat /tmp/usage_build.json)
 run_statistics=$(cat /tmp/usage_run.json)
 overall_stats="{\"exit_code\": $exit_code, \"usage\": {\"build\": $build_statistics, \"run\": $run_statistics}}"
-echo "$overall_stats" | put "$RUN_NAME" "$CLAY_INTERNAL_RUN_TOKEN" exit-data
+echo "$overall_stats" | put exit-data
 
 # Now take the filename given in $RUN_OUTPUT and save that away
 cd /app || exit
-put "$RUN_NAME" "$CLAY_INTERNAL_RUN_TOKEN" output < "$RUN_OUTPUT"
+put output < "$RUN_OUTPUT"
 
-finished "$RUN_NAME" "$CLAY_INTERNAL_RUN_TOKEN" run
-send-event "$RUN_NAME" "$CLAY_INTERNAL_RUN_TOKEN" "EOF"
+finished run
+send-event "EOF"
