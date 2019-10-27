4da381fd525e6918285f39e53c1c109661d05e24
Matthew Landauer <mlandauer@gmail.com>
2019-10-28 10:40:35 +1100

    Show the line number of a failure

--- cmd/clay-run/main.go
+++ cmd/clay-run/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+  "log"
+  
   "github.com/openaustralia/morph-ng/cmd/clay-run/cmd"
 )
 
 func main() {
+  // Show the source of the error with the standard logger. Don't show date & time
+	log.SetFlags(log.Lshortfile)
+
   cmd.Execute()
 }
