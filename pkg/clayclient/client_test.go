c2a0a3bc6c9ec85834f4d977622451fc07b26637
Matthew Landauer <mlandauer@gmail.com>
2019-10-29 15:54:23 +1100

    Add very naive first test of archiving

---
+++ pkg/clayclient/client_test.go
@@ -0,0 +1,69 @@
+package clayclient
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestArchive(t *testing.T) {
+	// First create a test directory which we want to archive
+	err := os.Mkdir("test", 0755)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = ioutil.WriteFile("test/foo.txt", []byte("foobar"), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create an archive
+	reader, err := createArchiveFromDirectory("test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	file, err := os.Create("test.tar.gz")
+	if err != nil {
+		t.Fatal(err)
+	}
+	io.Copy(file, reader)
+	file.Close()
+
+	// Extract the archive
+	file, err = os.Open("test.tar.gz")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = os.Mkdir("test2", 0755)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = extractArchiveToDirectory(file, "test2")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Now check the result
+	c, err := ioutil.ReadFile("test2/foo.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, "foobar", string(c))
+
+	// And finally tidy up
+	err = os.RemoveAll("test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = os.Remove("test.tar.gz")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = os.RemoveAll("test2")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
