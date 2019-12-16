543f90fa4dea66992be6100a80c9e86c7b4e6b54
Matthew Landauer <mlandauer@gmail.com>
2019-12-16 14:50:02 +1100

    Make keyvaluestore always handle strings

--- pkg/keyvaluestore/client.go
+++ pkg/keyvaluestore/client.go
@@ -2,7 +2,7 @@ package keyvaluestore
 
 // Client defines the interface to access the key value store
 type Client interface {
-	Set(key string, value interface{}) error
-	Get(key string) (interface{}, error)
+	Set(key string, value string) error
+	Get(key string) (string, error)
 	Delete(key string) error
 }
