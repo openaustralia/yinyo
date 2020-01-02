c8782c1f50c8588b1319c91b185d19be135233ff
Matthew Landauer <mlandauer@gmail.com>
2020-01-02 16:28:45 +1100

    Move blob store methods to their own file

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -24,9 +24,6 @@ import (
 	"github.com/openaustralia/yinyo/pkg/stream"
 )
 
-const filenameApp = "app.tgz"
-const filenameCache = "cache.tgz"
-const filenameOutput = "output"
 const dockerImage = "openaustralia/yinyo-scraper:v1"
 const runBinary = "/bin/yinyo"
 
@@ -373,31 +370,6 @@ func (app *AppImplementation) DeleteRun(runName string) error {
 // GetTokenCache gets the cached runToken. Returns ErrNotFound if run name doesn't exist
 func (app *AppImplementation) GetTokenCache(runName string) (string, error) {
 	return app.getKeyValueData(runName, tokenCacheKey)
-}
-
-func blobStoreStoragePath(runName string, fileName string) string {
-	return runName + "/" + fileName
-}
-
-func (app *AppImplementation) getBlobStoreData(runName string, fileName string) (io.Reader, error) {
-	p := blobStoreStoragePath(runName, fileName)
-	r, err := app.BlobStore.Get(p)
-	if err != nil && app.BlobStore.IsNotExist(err) {
-		return r, fmt.Errorf("blobstore %v: %w", p, ErrNotFound)
-	}
-	return r, err
-}
-
-func (app *AppImplementation) putBlobStoreData(reader io.Reader, objectSize int64, runName string, fileName string) error {
-	return app.BlobStore.Put(
-		blobStoreStoragePath(runName, fileName),
-		reader,
-		objectSize,
-	)
-}
-
-func (app *AppImplementation) deleteBlobStoreData(runName string, fileName string) error {
-	return app.BlobStore.Delete(blobStoreStoragePath(runName, fileName))
 }
 
 func (app *AppImplementation) postCallbackEvent(runName string, event event.Event) error {
