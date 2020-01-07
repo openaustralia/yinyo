5d52018cf88cced39df608258a1cf59e66c709c9
Matthew Landauer <mlandauer@gmail.com>
2020-01-07 16:24:38 +1100

    Extract method

--- pkg/apiclient/simple.go
+++ pkg/apiclient/simple.go
@@ -12,25 +12,7 @@ import (
 
 const cacheName = ".yinyo-build-cache.tgz"
 
-// Simple is a super simple high level way of running a scraper that exists on the local file system
-// and displaying the results to stdout/stderr. This is used by the command line client
-// It makes a simple common use case a little simpler to implement
-func Simple(scraperDirectory string, clientServerURL string, environment map[string]string, outputFile string, callbackURL string, showEventsJSON bool) error {
-	client := New(clientServerURL)
-	// Create the run
-	run, err := client.CreateRun(scraperDirectory)
-	if err != nil {
-		return err
-	}
-
-	// Upload the app
-	err = run.PutAppFromDirectory(scraperDirectory, []string{cacheName})
-	if err != nil {
-		return err
-	}
-
-	// Upload the cache
-	cachePath := filepath.Join(scraperDirectory, cacheName)
+func uploadCacheIfExists(run Run, cachePath string) error {
 	file, err := os.Open(cachePath)
 	if err != nil {
 		// If the cache doesn't exist then skip the uploading bit
@@ -44,13 +26,35 @@ func Simple(scraperDirectory string, clientServerURL string, environment map[str
 		}
 		file.Close()
 	}
+	return nil
+}
 
+// Simple is a super simple high level way of running a scraper that exists on the local file system
+// and displaying the results to stdout/stderr. This is used by the command line client
+// It makes a simple common use case a little simpler to implement
+func Simple(scraperDirectory string, clientServerURL string, environment map[string]string, outputFile string, callbackURL string, showEventsJSON bool) error {
+	client := New(clientServerURL)
+	// Create the run
+	run, err := client.CreateRun(scraperDirectory)
+	if err != nil {
+		return err
+	}
+	// Upload the app
+	err = run.PutAppFromDirectory(scraperDirectory, []string{cacheName})
+	if err != nil {
+		return err
+	}
+	// Upload the cache
+	cachePath := filepath.Join(scraperDirectory, cacheName)
+	err = uploadCacheIfExists(run, cachePath)
+	if err != nil {
+		return err
+	}
 	var envVariables []protocol.EnvVariable
 	for k, v := range environment {
 		// TODO: Fix this inefficient way
 		envVariables = append(envVariables, protocol.EnvVariable{Name: k, Value: v})
 	}
-
 	// Start the run
 	err = run.Start(&protocol.StartRunOptions{
 		Output:   outputFile,
@@ -60,7 +64,6 @@ func Simple(scraperDirectory string, clientServerURL string, environment map[str
 	if err != nil {
 		return err
 	}
-
 	// Listen for events
 	events, err := run.GetEvents("")
 	if err != nil {
@@ -71,17 +74,14 @@ func Simple(scraperDirectory string, clientServerURL string, environment map[str
 		if err != nil {
 			return err
 		}
-
 		err = displayEvent(e, showEventsJSON)
 		if err != nil {
 			return err
 		}
 	}
-
 	// Get the run output
 	if outputFile != "" {
-		path := filepath.Join(scraperDirectory, outputFile)
-		err = run.GetOutputToFile(path)
+		err = run.GetOutputToFile(filepath.Join(scraperDirectory, outputFile))
 		if err != nil {
 			if IsNotFound(err) {
 				log.Printf("Warning: output file %v does not exist", outputFile)
@@ -90,25 +90,11 @@ func Simple(scraperDirectory string, clientServerURL string, environment map[str
 			}
 		}
 	}
-
 	// Get the build cache
 	err = run.GetCacheToFile(cachePath)
 	if err != nil {
 		return err
 	}
-
-	// Get the exit data
-	// exitData, err := run.GetExitData()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// if exitData.Build != nil {
-	// 	fmt.Printf("Build: %+v\n", *exitData.Build)
-	// }
-	// if exitData.Run != nil {
-	// 	fmt.Printf("Run: %+v\n", *exitData.Run)
-	// }
-
 	// Delete the run
 	err = run.Delete()
 	if err != nil {
