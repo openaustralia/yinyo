9265b623856120a61f2ef4c72c66481fc81ae752
Matthew Landauer <mlandauer@gmail.com>
2020-01-13 14:28:31 +1100

    Small fix to documentation

--- pkg/apiclient/events.go
+++ pkg/apiclient/events.go
@@ -13,17 +13,17 @@ func (run *Run) CreateStartEvent(stage string) error {
 	return run.CreateEvent(protocol.NewStartEvent("", time.Now(), stage))
 }
 
-// CreateStartEvent creates and sends a "finish" event
+// CreateFinishEvent creates and sends a "finish" event
 func (run *Run) CreateFinishEvent(stage string) error {
 	return run.CreateEvent(protocol.NewFinishEvent("", time.Now(), stage))
 }
 
-// CreateStartEvent creates and sends a "log" event
+// CreateLogEvent creates and sends a "log" event
 func (run *Run) CreateLogEvent(stage string, stream string, text string) error {
 	return run.CreateEvent(protocol.NewLogEvent("", time.Now(), stage, stream, text))
 }
 
-// CreateStartEvent creates and sends a "last" event
+// CreateLastEvent creates and sends a "last" event
 func (run *Run) CreateLastEvent() error {
 	return run.CreateEvent(protocol.NewLastEvent("", time.Now()))
 }
