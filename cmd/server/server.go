0d625dbb7c366f40f24bcc0d8f24c693b5d2110d
Matthew Landauer <mlandauer@gmail.com>
2020-02-25 11:49:34 +1100

    Simplify how binaries are put together

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,14 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
-	"github.com/openaustralia/yinyo/cmd/server/cmd"
+	"github.com/openaustralia/yinyo/pkg/apiserver"
+	"github.com/openaustralia/yinyo/pkg/commands"
+	"github.com/spf13/cobra"
 )
 
 func main() {
 	// Show the source of the error with the standard logger. Don't show date & time
 	log.SetFlags(log.Lshortfile)
 
-	cmd.Execute()
+	var maxRunTime int64
+
+	var rootCmd = &cobra.Command{
+		Use:   "server",
+		Short: "Serves the Yinyo API",
+		Run: func(cmd *cobra.Command, args []string) {
+			server := apiserver.Server{}
+			minioOptions := commands.MinioOptions{
+				Host:      os.Getenv("STORE_HOST"),
+				Bucket:    os.Getenv("STORE_BUCKET"),
+				AccessKey: os.Getenv("STORE_ACCESS_KEY"),
+				SecretKey: os.Getenv("STORE_SECRET_KEY"),
+			}
+			redisOptions := commands.RedisOptions{
+				Address:  "redis:6379",
+				Password: os.Getenv("REDIS_PASSWORD"),
+			}
+			err := server.Initialise(&commands.StartupOptions{Minio: minioOptions, Redis: redisOptions}, maxRunTime)
+			if err != nil {
+				log.Fatal(err)
+			}
+			server.Run(":8080")
+		},
+	}
+
+	rootCmd.Flags().Int64Var(&maxRunTime, "maxruntime", 86400, "Set the global maximum run time in seconds that all runs can not exceed")
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- cmd/yinyo/yinyo.go
+++ cmd/yinyo/yinyo.go
@@ -1,14 +1,84 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"os"
 
-	"github.com/openaustralia/yinyo/cmd/yinyo/cmd"
+	"github.com/openaustralia/yinyo/pkg/apiclient"
+	"github.com/openaustralia/yinyo/pkg/protocol"
+	"github.com/spf13/cobra"
 )
+
+// Convert the internal text representation of a stream type ("stdout"/"stderr") to the go stream
+// we can write to
+func osStream(stream string) (*os.File, error) {
+	switch stream {
+	// TODO: Extract string constant
+	case "stdout":
+		return os.Stdout, nil
+	case "stderr", "interr":
+		return os.Stderr, nil
+	default:
+		return nil, fmt.Errorf("unexpected stream %v", stream)
+	}
+}
 
 func main() {
 	// Show the source of the error with the standard logger. Don't show date & time
 	log.SetFlags(log.Lshortfile)
 
-	cmd.Execute()
+	var callbackURL, outputFile, clientServerURL string
+	var showEventsJSON bool
+	var environment map[string]string
+
+	var rootCmd = &cobra.Command{
+		Use:   "yinyo scraper_directory",
+		Short: "Yinyo runs heaps of scrapers easily, fast and scalably",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			scraperDirectory := args[0]
+			err := apiclient.Simple(
+				scraperDirectory, clientServerURL, environment, outputFile, callbackURL,
+				func(event protocol.Event) error {
+					if showEventsJSON {
+						// Convert the event back to JSON for display
+						b, err := json.Marshal(event)
+						if err != nil {
+							return err
+						}
+						fmt.Println(string(b))
+					} else {
+						// Only display the log events to the user
+						l, ok := event.Data.(protocol.LogData)
+						if ok {
+							f, err := osStream(l.Stream)
+							if err != nil {
+								return err
+							}
+							fmt.Fprintln(f, l.Text)
+						}
+					}
+					return nil
+				},
+			)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+		},
+	}
+
+	rootCmd.Flags().StringVar(&callbackURL, "callback", "", "Optionally provide a callback URL. For every event a POST to the URL will be made. To be able to authenticate the callback you'll need to specify a secret in the URL. Something like http://my-url-endpoint.com?key=special-secret-stuff would do the trick")
+	// TODO: Check that the output file is a relative path and if not error
+	rootCmd.Flags().StringVar(&outputFile, "output", "", "The output is written to the same local directory at the end. The output file path is given relative to the scraper directory")
+	rootCmd.Flags().StringVar(&clientServerURL, "server", "http://localhost:8080", "Override yinyo server URL")
+	rootCmd.Flags().StringToStringVar(&environment, "env", map[string]string{}, "Set one or more environment variables (e.g. --env foo=twiddle,bar=blah)")
+	rootCmd.Flags().BoolVar(&showEventsJSON, "eventsjson", false, "Show the full events output as JSON instead of the default of just showing the log events as text")
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
