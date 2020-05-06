84699dbc5e2d3d91a65c2208beae2bbe4a9860ef
Matthew Landauer <mlandauer@gmail.com>
2020-05-06 10:39:23 +1000

    Can now reconnect in client to already running run

--- cmd/yinyo/yinyo.go
+++ cmd/yinyo/yinyo.go
@@ -26,33 +26,26 @@ func osStream(stream string) (*os.File, error) {
 	}
 }
 
-func run(scraperDirectory string, clientServerURL string, environment map[string]string,
-	outputFile string, cache bool, callbackURL string, showEventsJSON bool, apiKey string) error {
-	return apiclient.Simple(
-		scraperDirectory, clientServerURL, environment, outputFile, cache, callbackURL, apiKey,
-		func(event protocol.Event) error {
-			if showEventsJSON {
-				// Convert the event back to JSON for display
-				b, err := json.Marshal(event)
-				if err != nil {
-					return err
-				}
-				fmt.Println(string(b))
-			} else {
-				// Only display the log events to the user
-				l, ok := event.Data.(protocol.LogData)
-				if ok {
-					f, err := osStream(l.Stream)
-					if err != nil {
-						return err
-					}
-					fmt.Fprintln(f, l.Text)
-				}
+func display(event protocol.Event, showEventsJSON bool) error {
+	if showEventsJSON {
+		// Convert the event back to JSON for display
+		b, err := json.Marshal(event)
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(b))
+	} else {
+		// Only display the log events to the user
+		l, ok := event.Data.(protocol.LogData)
+		if ok {
+			f, err := osStream(l.Stream)
+			if err != nil {
+				return err
 			}
-			return nil
-		},
-	)
-
+			fmt.Fprintln(f, l.Text)
+		}
+	}
+	return nil
 }
 
 func apiKeysPath() (string, error) {
@@ -104,7 +97,7 @@ func main() {
 	// Show the source of the error with the standard logger. Don't show date & time
 	log.SetFlags(log.Lshortfile)
 
-	var callbackURL, outputFile, clientServerURL string
+	var callbackURL, outputFile, clientServerURL, connectToRunID string
 	var showEventsJSON, cache bool
 	var environment map[string]string
 
@@ -114,31 +107,43 @@ func main() {
 		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			scraperDirectory := args[0]
-			apiKeys, err := loadAPIKeys()
-			if err != nil {
-				log.Fatal(err)
-			}
-			for {
-				err := run(scraperDirectory, clientServerURL, environment, outputFile, cache, callbackURL, showEventsJSON, apiKeys[clientServerURL])
+
+			if connectToRunID == "" {
+				apiKeys, err := loadAPIKeys()
 				if err != nil {
-					// If get unauthorized error back then we should let the user enter their api key and try again
-					// And we should save away the api key (for this particular server) for later use
-					if apiclient.IsUnauthorized(err) {
-						fmt.Print("Enter your api key: ")
-						var apiKey string
-						fmt.Scanln(&apiKey)
-						apiKeys[clientServerURL] = apiKey
-						err = saveAPIKeys(apiKeys)
-						if err != nil {
+					log.Fatal(err)
+				}
+				apiKey := apiKeys[clientServerURL]
+				for {
+					err := apiclient.Simple(
+						scraperDirectory, clientServerURL, environment, outputFile, cache, callbackURL, apiKey,
+						func(event protocol.Event) error { return display(event, showEventsJSON) },
+					)
+					if err != nil {
+						// If get unauthorized error back then we should let the user enter their api key and try again
+						// And we should save away the api key (for this particular server) for later use
+						if apiclient.IsUnauthorized(err) {
+							fmt.Print("Enter your api key: ")
+							var apiKey string
+							fmt.Scanln(&apiKey)
+							apiKeys[clientServerURL] = apiKey
+							err = saveAPIKeys(apiKeys)
+							if err != nil {
+								log.Fatal(err)
+							}
+						} else {
 							log.Fatal(err)
 						}
 					} else {
-						log.Fatal(err)
+						break
 					}
-				} else {
-					break
 				}
-
+			} else {
+				// Reconnect to existing run
+				err := apiclient.ConnectToStartedRun(connectToRunID, scraperDirectory, clientServerURL, outputFile, cache, func(event protocol.Event) error { return display(event, showEventsJSON) })
+				if err != nil {
+					log.Fatal(err)
+				}
 			}
 		},
 	}
@@ -146,6 +151,7 @@ func main() {
 	rootCmd.Flags().StringVar(&callbackURL, "callback", "", "Optionally provide a callback URL. For every event a POST to the URL will be made. To be able to authenticate the callback you'll need to specify a secret in the URL. Something like http://my-url-endpoint.com?key=special-secret-stuff would do the trick")
 	// TODO: Check that the output file is a relative path and if not error
 	rootCmd.Flags().StringVar(&outputFile, "output", "", "The output is written to the same local directory at the end. The output file path is given relative to the scraper directory")
+	rootCmd.Flags().StringVar(&connectToRunID, "connect", "", "Connect to a run that has already started by giving the run ID")
 	rootCmd.Flags().StringVar(&clientServerURL, "server", "http://localhost:8080", "Override yinyo server URL")
 	rootCmd.Flags().StringToStringVar(&environment, "env", map[string]string{}, "Set one or more environment variables (e.g. --env foo=twiddle,bar=blah)")
 	rootCmd.Flags().BoolVar(&showEventsJSON, "allevents", false, "Show the full events output as JSON instead of the default of just showing the log events as text")
